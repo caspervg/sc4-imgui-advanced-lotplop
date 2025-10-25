ec3890862d20b13278b251243b76c36205ca95e2
Casper Van Gheluwe <caspervg@gmail.com>
2025-10-25 19:16:39 +0200

    chore: bump version

--- src/version.h
+++ src/version.h
@@ -21,6 +21,6 @@
 
 #pragma once
 
-#define PLUGIN_VERSION_STR	    "0.1.2"
-#define RESOURCE_VERSION         0,1,2,0
-#define RESOURCE_VERSION_STR    "0.1.2.0"
+#define PLUGIN_VERSION_STR	    "0.1.3"
+#define RESOURCE_VERSION         0,1,3,0
+#define RESOURCE_VERSION_STR    "0.1.3.0"
